9213aff6250738f40e5b50174235c0341ee0095f
Xu Zhiyong <13980668829@163.com>
2020-10-03 22:09:01 +0800

    Improve Geometry class to support Ray, Sphere, Plane as well as Point, Circle, Cylinder. Also there is Vector class inside.

--- app/src/main/java/com/focus617/myopengldemo/util/Geometry.kt
+++ app/src/main/java/com/focus617/myopengldemo/util/Geometry.kt
@@ -7,8 +7,19 @@ class Geometry {
         fun translateY(distance: Float): Point {
             return Point(x, y + distance, z)
         }
+
+        fun translate(vector: Vector): Point {
+            return Point(
+                x + vector.x,
+                y + vector.y,
+                z + vector.z
+            )
+        }
     }
 
+    class Ray(val point: Point, val vector: Vector)
+
+    // TODO: Re-use shared stuff in classes as an exercise
     class Circle(val center: Point, val radius: Float) {
 
         fun scale(scale: Float): Circle {
@@ -17,4 +28,77 @@ class Geometry {
     }
 
     class Cylinder(val center: Point, val radius: Float, val height: Float)
+
+    class Sphere(val center: Point, val radius: Float)
+
+    class Plane(val point: Point, val normal: Vector)
+
+
+    companion object {
+
+        class Vector(val x: Float, val y: Float, val z: Float) {
+
+            fun length(): Float = kotlin.math.sqrt(x * x + y * y + z * z)
+
+            // http://en.wikipedia.org/wiki/Cross_product
+            fun crossProduct(other: Vector) = Vector(
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x
+            )
+
+            // http://en.wikipedia.org/wiki/Dot_product
+            fun dotProduct(other: Vector): Float = x * other.x + y * other.y + z * other.z
+
+            fun scale(f: Float) = Vector(
+                x * f,
+                y * f,
+                z * f
+            )
+        }
+
+        fun vectorBetween(from: Point, to: Point): Vector {
+            return Vector(
+                to.x - from.x,
+                to.y - from.y,
+                to.z - from.z
+            )
+        }
+
+        fun intersects(sphere: Sphere, ray: Ray): Boolean {
+            return Geometry.distanceBetween(sphere.center, ray) < sphere.radius
+        }
+
+        // http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
+        // Note that this formula treats Ray as if it extended infinitely past
+        // either point.
+        fun distanceBetween(point: Point, ray: Ray): Float {
+            val p1ToPoint: Vector = Geometry.vectorBetween(ray.point, point)
+            val p2ToPoint: Vector = Geometry.vectorBetween(ray.point.translate(ray.vector), point)
+
+            // The length of the cross product gives the area of an imaginary
+            // parallelogram having the two vectors as sides. A parallelogram can be
+            // thought of as consisting of two triangles, so this is the same as
+            // twice the area of the triangle defined by the two vectors.
+            // http://en.wikipedia.org/wiki/Cross_product#Geometric_meaning
+            val areaOfTriangleTimesTwo = p1ToPoint.crossProduct(p2ToPoint).length()
+            val lengthOfBase = ray.vector.length()
+
+            // The area of a triangle is also equal to (base * height) / 2. In
+            // other words, the height is equal to (area * 2) / base. The height
+            // of this triangle is the distance from the point to the ray.
+            return areaOfTriangleTimesTwo / lengthOfBase
+        }
+
+        // http://en.wikipedia.org/wiki/Line-plane_intersection
+        // This also treats rays as if they were infinite. It will return a
+        // point full of NaNs if there is no intersection point.
+        fun intersectionPoint(ray: Ray, plane: Geometry.Plane): Point {
+            val rayToPlaneVector: Vector = Geometry.vectorBetween(ray.point, plane.point)
+            val scaleFactor = (rayToPlaneVector.dotProduct(plane.normal)
+                    / ray.vector.dotProduct(plane.normal))
+            return ray.point.translate(ray.vector.scale(scaleFactor))
+        }
+
+    }
 }
