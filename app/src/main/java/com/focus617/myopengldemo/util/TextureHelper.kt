3060f82ad6076ff5f2fc7b6a26c4cdd476f61ddf
Xu Zhiyong <13980668829@163.com>
2020-10-08 13:58:42 +0800

    Refact MultiTexture Setting.

--- app/src/main/java/com/focus617/myopengldemo/util/TextureHelper.kt
+++ app/src/main/java/com/focus617/myopengldemo/util/TextureHelper.kt
@@ -145,19 +145,44 @@ object TextureHelper {
                 glTexParameterf(GL_TEXTURE_2D, TEXTURE_MAX_ANISOTROPY_EXT, 1.0f)
             }
         }
-        if (filterMode == FilterMode.NEAREST_NEIGHBOUR) {
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        } else {
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-            if (filterMode == FilterMode.BILINEAR) {
+
+//        if (filterMode == FilterMode.NEAREST_NEIGHBOUR) {
+//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+//        } else {
+//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+//            if (filterMode == FilterMode.BILINEAR) {
+//                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+//            } else if (filterMode == FilterMode.BILINEAR_WITH_MIPMAPS) {
+//                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST)
+//            } else {
+//                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+//            }
+//        }
+        when(filterMode){
+            FilterMode.NEAREST_NEIGHBOUR -> {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            }
+            FilterMode.BILINEAR -> {
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-            } else if (filterMode == FilterMode.BILINEAR_WITH_MIPMAPS) {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            }
+            FilterMode.BILINEAR_WITH_MIPMAPS -> {
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST)
-            } else {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            }
+            FilterMode.TRILINEAR -> {
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            }
+            FilterMode.ANISOTROPIC -> {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
             }
         }
+
         glBindTexture(GL_TEXTURE_2D, 0)
     }
+
 }
